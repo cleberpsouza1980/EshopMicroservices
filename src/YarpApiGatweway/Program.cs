77d61a3ccd3d4bef7cf2ba322496adb6b7482067
cleber de souza <caps0646@gmail.com>
2026-03-19 18:30:12 -0300

    Implementacao

--- src/YarpApiGatweway/Program.cs
+++ src/YarpApiGatweway/Program.cs
@@ -1,15 +1,26 @@
+using Microsoft.AspNetCore.RateLimiting;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));  
 
+builder.Services.AddRateLimiter(rateLimitOptions =>
+{
+    rateLimitOptions.AddFixedWindowLimiter("fixed", options =>
+        {
+            options.Window = TimeSpan.FromSeconds(10); // Time window duration
+            options.PermitLimit = 5; // Maximum number of requests allowed in the time window
+        });
+});
 
 var app = builder.Build();
 
 
 //Configure the HTTP request pipeline.
-app.MapReverseProxy();
+app.UseRateLimiter(); // Enable rate limiting middleware
 
+app.MapReverseProxy();
 
 app.Run();
